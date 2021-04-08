9abcf273f3fc19ed2fafe65ad4a6b59b356f7f2e
Manan Gupta <manangreen8877@gmail.com>
2021-04-08 10:38:06 +0530

    Create sumofeven.java

---
+++ lec3/sumofeven.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i=1;
+        int sum=0;
+        while(i<=n){
+            if(i%2==0){
+             sum = sum + i;
+                
+            }
+            i++;
+        }
+        System.out.println(sum);
+		
+		
+	}
+
+}
