f3220ced3d1cb159ffebf9887ba347df36c93cef
Manan Gupta <manangreen8877@gmail.com>
2021-04-19 21:49:04 +0530

    Create Array1Use.java

---
+++ lec8/Array1Use.java
@@ -0,0 +1,18 @@
+public class ArrayUse {
+	
+	public static void main(String[] args) {
+		int n = 10;
+		int[] arr = new int[n];
+//		arr[0] = 15;
+		System.out.println(arr[0]);
+		System.out.println(arr[5]);
+		
+		char chArray[] = new char[11];
+		chArray[0] = 'a';
+		double dArray[] = new double[12];
+		dArray[0] = 1.2;
+		System.out.println(chArray[0]);
+		System.out.println(dArray[0]);
+		
+	}
+
