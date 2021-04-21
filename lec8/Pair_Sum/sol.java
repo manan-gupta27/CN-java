e1f035e4978e71cc5da7753f26686aa85dab7e05
Manan Gupta <manangreen8877@gmail.com>
2021-04-21 23:37:35 +0530

    Create sol.java

---
+++ lec8/Pair_Sum/sol.java
@@ -0,0 +1,14 @@
+public class Solution {  
+
+    public static int pairSum(int arr[], int x) {
+        int count=0;
+    	for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]+arr[j]==x){
+                    count++;
+                }
+            }
+        }
+        return count/2;
+    }
+}
