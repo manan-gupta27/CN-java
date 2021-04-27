679a187f669b62bf600690bdd928eee9ca7944ad
Manan Gupta <manangreen8877@gmail.com>
2021-04-27 14:04:58 +0530

    Create sol.java

---
+++ lec9-assignment/Sort_0_1_2/sol.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+
+    public static void sort012(int[] arr){
+    	int n=arr.length;
+        int nz=0;
+        int nt=n-1;
+        int i=0;
+		while(i<=nt){
+            if(arr[i]==0){
+                int temp1=arr[nz];
+                arr[nz]=arr[i];
+                arr[i]=temp1;
+                nz++;
+                i++;
+            }else if(arr[i]==2){
+                int temp2=arr[nt];
+                arr[nt]=arr[i];
+                arr[i]=temp2;               
+                nt--;
+            }else{
+                i++;
+        }
+        }
+    }
+
+}
