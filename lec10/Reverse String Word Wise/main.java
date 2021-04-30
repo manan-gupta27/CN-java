e64c4b43c205d338ee61c8f7935912432e66ce48
Manan Gupta <manangreen8877@gmail.com>
2021-04-30 23:59:46 +0530

    Create main.java

---
+++ lec10/Reverse String Word Wise/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		System.out.println(Solution.reverseWordWise(input));
+	}
+}
