51a6ffde95d141f99eb8d83388a9d8162dd6948d
Manan Gupta <manangreen8877@gmail.com>
2021-05-03 21:52:17 +0530

    Create main.java

---
+++ lec10-assignment/Remove_Consecutive_Duplicates/main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+	public static void main (String [] args) throws NumberFormatException, IOException {
+		String str = br.readLine();
+		if (str != null) {
+			str = str.trim ();
+		} else {
+			str = "";
+		}
+
+		String ans = Solution.removeConsecutiveDuplicates(str);
+
+		System.out.println(ans);
+		
+	}
+}
