10a0b0ea7364c927a06b1b41e53ef6cd206d85fd
Manan Gupta <manangreen8877@gmail.com>
2021-08-03 22:17:48 +0530

    Create sumofnodes.java

---
+++ trees/sumofnodes.java
@@ -0,0 +1,30 @@
+
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	
+	
+	public static int sumOfAllNode(TreeNode<Integer> root){
+	if(root==null){
+        return 0;
+    }
+        int sum=0;
+        
+        for(int i=0;i<root.children.size();i++){
+           sum=sum + sumOfAllNode(root.children.get(i));
+        }
+        return root.data+sum;
+	}
+}
+
