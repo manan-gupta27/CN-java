27147a6483416fe649f765a11c181f16fe116a44
Manan Gupta <manangreen8877@gmail.com>
2021-08-07 22:29:47 +0530

    Create sortedArrayToBST.java

---
+++ Binary Search Trees/sortedArrayToBST.java
@@ -0,0 +1,40 @@
+public class Solution {
+
+	/*	Binary Tree Node class 
+	 * 
+	 * 	class BinaryTreeNode<T> {
+			T data;
+			BinaryTreeNode<T> left;
+			BinaryTreeNode<T> right;
+			
+			public BinaryTreeNode(T data) {
+				this.data = data;
+			}
+		}
+		*/
+	 public static BinaryTreeNode<Integer> sortedArrayToBST(int arr[], int start, int end) { 
+  
+        /* Base Case */
+        if (start > end) { 
+            return null; 
+        } 
+  
+        /* Get the middle element and make it root */
+        int mid = (start + end) / 2; 
+        BinaryTreeNode<Integer> node = new BinaryTreeNode<Integer>(arr[mid]); 
+  
+        /* Recursively construct the left subtree and make it 
+         left child of root */
+        node.left = sortedArrayToBST(arr, start, mid - 1); 
+  
+        /* Recursively construct the right subtree and make it 
+         right child of root */
+        node.right = sortedArrayToBST(arr, mid + 1, end); 
+          
+        return node; 
+    } 
+		public static BinaryTreeNode<Integer> SortedArrayToBST(int[] arr, int n){
+			return sortedArrayToBST(arr,0,arr.length-1);
+            
+		}
+	}
