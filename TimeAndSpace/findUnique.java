a21b1c7df74bbb26b4e410347db806836df9fac1
Manan Gupta <manangreen8877@gmail.com>
2021-08-09 22:50:04 +0530

    Create findUnique.java

---
+++ TimeAndSpace/findUnique.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+	public static int findUnique(int[] arr) {
+		   int res = 0; 
+        int n=arr.length-1;
+        for (int i = 0; i <= n ; i++) 
+        { 
+            res=res ^ arr[i]; 
+        }
+      
+	
+    return res;
+        
+    
+	}
+}
